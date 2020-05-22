d1d0846c9e7801031e4c3fc708f7650d6f00da52
Rexarrior <Rexarrior@yandex.ru>
2020-05-22 23:12:35 +0300

    fix frontend

--- frontend/plot-maker/src/components/Workspace.vue
+++ frontend/plot-maker/src/components/Workspace.vue
@@ -71,9 +71,9 @@
             ></b-form-input>
           </b-form-group>
 
-          <b-form-group id="input-group-3" label="Список переменных" label-for="input-2">
+          <b-form-group id="input-group-3" label="Список переменных" label-for="input-3">
             <b-form-input
-              id="input-2"
+              id="input-3"
               v-model="RawVariables"
               required
               placeholder="x:1,0.01, 10; y:-100, 10, 100; "
@@ -91,7 +91,13 @@
           </b-form>
         </b-tab>
         <b-tab title="Результаты" v-if="IsResults" class="results-table">
-           <b-table hover :items="Results" ></b-table>
+           <b-container fluid class="table-container">
+            <b-table
+              :items="Results" 
+              caption-top
+            >
+            </b-table>
+           </b-container>
           </b-tab>
       </b-tabs>
     </b-jumbotron>
@@ -179,7 +185,7 @@ export default {
       this.Expression.variables = [];
       this.RawVariables = "";
       this.Results = [];
-      this.IsResults = true;
+      this.IsResults = false;
 
      },
 
@@ -299,28 +305,112 @@ export default {
 
 
 .expr-list-card{
-  position: fixed;
+  position: absolute;
   left: 5%;
   top: 10%;
+  bottom: 5%;
   padding-bottom: 10%;
   width: 25%;
-  height: 70%;
   font-size: 20;
-  overflow: scroll;
 
 }
 .expr-work-area{
   position: absolute;
-  width: 50%;
-  height: 70%;
-  max-height: 70%;
-  left:35%;
-  top:10%;
+  left: 35%;
+  top: 10%;
+  bottom: 5%;
   padding-bottom: 10%;
+  width: 45%;
+  font-size: 20;
 }
 
-.results-table{
-  overflow:scroll ;
-  
+.table-container {
+     height: calc(100vh - 450px);
 }
+ .table-container table {
+     display: flex;
+     flex-flow: column;
+     height: 100%;
+     width: 100%;
+}
+ .table-container table thead {
+     flex: 0 0 auto;
+     width: 100%;
+}
+ .table-container table tbody {
+     flex: 1 1 auto;
+     display: block;
+     width: 100%;
+     overflow-y: auto;
+}
+ .table-container table tbody tr {
+     width: 100%;
+}
+ .table-container table thead, .table-container table tbody tr {
+     display: table;
+     table-layout: fixed;
+}
+ .table-container table {
+     border-collapse: collapse;
+}
+ .table-container table td, .table-container table th {
+     padding: 0.4em;
+}
+ .table-container table th {
+     border: 1px solid black;
+     font-size: 0.7vw;
+}
+ .table-container table td {
+     border: 1px solid #e7e1e1;
+     font-size: 0.85em;
+    /* necessary to set for proper "showing row x of y" calculations if infinate scoll */
+     white-space: nowrap;
+     text-align: center;
+     padding: 10px 5px;
+     white-space: nowrap;
+     text-overflow: ellipsis;
+}
+ .table-container table thead {
+     border: 2px solid #0F0FA3;
+}
+ .table-container th {
+     background-color: #0F0FA3;
+     color: #b5aba4;
+     cursor: pointer;
+     -webkit-user-select: none;
+   -moz-user-select: none !important;
+     -ms-user-select: none;
+     user-select: none;
+}
+ .table-container table tbody td {
+     padding: 8px;
+     cursor: pointer;
+}
+ .table-container table tbody tr:hover {
+     background-color: rgba(168, 168, 239, .5);
+}
+ .table-container table thead td {
+     padding: 10px 5px;
+}
+ .table-container tr:nth-child(even) {
+     background-color: rgba(168, 168, 239, 1);
+}
+
+/* START Adjustments for width and scrollbar hidden */
+ .table-container th.table-action, .table-container td.table-action {
+     width: 5.8vw;
+}
+ .table-container table thead {
+     width: calc(100% - 1px);
+}
+ .table-container table tbody::-webkit-scrollbar {
+     width: 1px;
+}
+ .table-container table tbody::-webkit-scrollbar {
+     width: 1px;
+}
+ .table-container table tbody::-webkit-scrollbar-thumb {
+     width: 1px;
+}
+/* END Adjustments for width and scrollbar */
 </style>
